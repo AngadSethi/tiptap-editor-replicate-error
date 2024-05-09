d6c7cc0a49af7df4ebc4d8fdd34e59f5380a6afe
Angad Sethi <angadsethi18@gmail.com>
2024-05-09 15:57:31 +0530

    Add Relations

--- app/Filament/Resources/ArticleResource/RelationManagers/CommentsRelationManager.php
+++ app/Filament/Resources/ArticleResource/RelationManagers/CommentsRelationManager.php
@@ -19,35 +19,7 @@ class CommentsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                TiptapEditor::make('content')->required()->columnSpanFull()->default("<pre><code>**Solution**
-
-The correct answer is **Option B) ₹72 Lakhs.**
-
-First, let&#039;s determine the total sales for each broker:
-- **Broker A**: Each house sells for ₹24,00,000. Selling 50 houses, total sales are:
-  $$
-  24,00,000 \times 50 = ₹12\text{ crores}
-  $$
-- **Broker B**: Each house sells for ₹20,00,000. Selling 45 houses, total sales are:
-  $$
-  20,00,000 \times 45 = ₹9\text{ crores}
-  $$
-
-Next, we calculate their earnings based on their commission rates:
-- **Broker A**: Receives a 3% commission. His earnings are:
-  $$
-  \frac{3}{100} \times 120000000 = ₹36,00,000
-  $$
-- **Broker B**: Receives a 4% commission. His earnings are:
-  $$
-  \frac{4}{100} \times 90000000 = ₹36,00,000
-  $$
-
-**Total earnings for both brokers combined**:
-$$
-₹36,00,000 + ₹36,00,000 = ₹72,00,000 \text{ or ₹72 Lakhs.}
-$$
-</code></pre></code></pre>")
+                TiptapEditor::make('text')->required()->columnSpanFull()
             ]);
     }
 
